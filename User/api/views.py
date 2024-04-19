1ae4b2098a391f80551225098c0e4f0b6a384e5b
abhinz <abhinandorg3@gmail.com>
2024-04-19 16:20:11 +0530

    created logout view

--- User/api/views.py
+++ User/api/views.py
@@ -63,48 +63,30 @@ class LoginView(APIView):
 
 
 
-
-
-
-
-
-
-
-
 from rest_framework_simplejwt.tokens import RefreshToken
 
-# class LogoutView(APIView):
-#     def post(self, request):
-#         try:
-#             refresh_token = request.data.get("refresh")
-#             print('dfbsdjbvjkbsjkdbnkjnsdkjfnksjndfnk')
-#             if refresh_token:
-#                 # Blacklist the refresh token
-#                 token = RefreshToken(refresh_token)
-#                 token.blacklist()
-#                 print('Successfully logged out')
-#                 return Response({"message": "Successfully logged out"}, status=status.HTTP_205_RESET_CONTENT)
-#             else:
-#                 return Response({"error": "Refresh token not provided"}, status=status.HTTP_400_BAD_REQUEST)
-#         except Exception as e:
-#             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class LogoutView(APIView):
-    permission_classes = (AllowAny,)
-    authentication_classes = ()
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request):
         try:
-            refresh_token = request.data["refresh_token"]
+            refresh_token = request.data["refresh"]
+            print(refresh_token)
             token = RefreshToken(refresh_token)
             token.blacklist()
 
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+        
 
 
+
+
+        
 
 class UserDetails(APIView):
     permission_classes = [IsAuthenticated]
