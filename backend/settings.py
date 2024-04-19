890844b3b494b19264f33d1e0feeb91314ba7218
abhinz <abhinandorg3@gmail.com>
2024-04-19 08:28:48 +0530

    added

--- backend/settings.py
+++ backend/settings.py
@@ -77,21 +77,23 @@ TEMPLATES = [
 # ASGI_APPLICATION = 'backend.asgi.application'
 
 
-# CHANNEL_LAYERS = {
-#     "default":{
-#         "BACKEND":"channels_redis.core.RedisChannelLayer",
-#         "CONFIG":{"hosts":[("127.0.0.1",6379)]},
-#     }
-# }
+CHANNEL_LAYERS = {
+    'default': {
+       'BACKEND': 'channels_redis.core.RedisChannelLayer',
+       'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 ASGI_APPLICATION = 'backend.asgi.application'
 
-CHANNEL_LAYERS = {
-    'default':{
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
-    }
-}
+# CHANNEL_LAYERS = {
+#     'default':{
+#         'BACKEND':'channels.layers.InMemoryChannelLayer'
+#     }
+# }
 
 
 
